888978acf75dcd8c30b42b9f153995bd19cb1adc
ArielRenée <rios94.a@gmail.com>
2020-03-26 20:54:20 -0700

    added helper functions

--- booktonica/react-app/src/helpers/booktonica-api-fetcher.js
+++ booktonica/react-app/src/helpers/booktonica-api-fetcher.js
@@ -1,5 +1,12 @@
+function generateErrorMessage(path, resp) {
+  return `😩 fetch('${path}') failed: Express server responded with HTTP: [${resp.status} ${resp.statusText}].
+  
+  (Note: this error is custom to Booktonica and you cannot Google it). Check your Network console for more information about the request and the Express logs for more information about the response.`;
+}
+
 export function getAllBooks() {
-  return fetch("/books", {
+  const path = "/books";
+  return fetch(path, {
     headers: {
       // This header is needed or React app won't proxy it along to Express
       Accept: "application/json"
@@ -8,8 +15,7 @@ export function getAllBooks() {
     if (resp.ok) {
       return resp.json();
     } else {
-      throw new Error(
-        `😩 fetch('/books') failed: Express server responded with HTTP ${resp.status} ${resp.statusText}. (Note: this error is custom to Booktonica and you cannot Google it). Check your Network console for more information about the request and the Express logs for more information about the response.`
+      throw new Error(generateErrorMessage(path, resp)
       );
     }
   });
@@ -26,9 +32,61 @@ export function getBooksByGenre(genre) {
     if (resp.ok) {
       return resp.json();
     } else {
-      throw new Error(
-        `😩 fetch('/books/genres/:genres') failed: Express server responded with HTTP ${resp.status} ${resp.statusText}. (Note: this error is custom to Booktonica and you cannot Google it). Check your Network console for more information about the request and the Express logs for more information about the response.`
+      throw new Error(generateErrorMessage(path, resp)
       );
     }
   });
 }
+
+export function sortNewFirst() {
+  const path = '/books/newest';
+  return fetch(path, {
+    headers: {
+      // This header is needed or React app won't proxy it along to Express
+      Accept: "application/json"
+    }
+  }).then(resp => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw new Error(generateErrorMessage(path, resp)
+      );
+    }
+  });
+}
+
+export function sortOldFirst() {
+  const path = '/books/oldest';
+  return fetch(path, {
+    headers: {
+      // This header is needed or React app won't proxy it along to Express
+      Accept: "application/json"
+    }
+  }).then(resp => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw new Error(generateErrorMessage(path, resp)
+      );
+    }
+  });
+}
+
+export function sortByABC() {
+  const path = '/books/alphabetically';
+  return fetch(path, {
+    headers: {
+      // This header is needed or React app won't proxy it along to Express
+      Accept: "application/json"
+    }
+  }).then(resp => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw new Error(generateErrorMessage(path, resp)
+      );
+    }
+  });
+}
+
+
